af0b12aa44c46f7c4cd264161f17b4eb39de3658
malinha22 <bruga.amalia@escola.pr.gov.br>
2025-08-04 19:39:49 -0300

    index

---
+++ assets/pergunta.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Mato Grosso?',
+    'A capital do Mato Grosso é Cuiabá'
+)
+
+criaCartao(
+    'Fronteiras políticas',
+    'Qual é o nome do rio que faz fronteira com Mato Grosso?',
+    'Rio Anarguaia'
+)
+    criaCartao(
+    'Lingua inglesa',
+    'Como se diz boa noita em inglês?',
+    'Boa noite em inglês é goodnight'
+)
